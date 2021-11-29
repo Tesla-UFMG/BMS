69422219090e918ccb180caf19c62811e3e90d60
Thiago Santos <thiagosantos.on@gmail.com>
2021-11-29 09:01:53 -0300

    atualizando função do nextion

--- Src/nextion_functions.c
+++ Src/nextion_functions.c
@@ -7,6 +7,10 @@ extern UART_HandleTypeDef huart3;
 extern uint8_t uart_user_message[256];	/* Buffer received for user access */
 uint8_t stat = 0;
 extern uint16_t balance_timer;
+uint8_t flag_information_to_send = 0;
+//NextionPage_e previous_page = PAGE0;
+//NextionPage_e actual_page = PAGE0;
+//NextionAdvice_e actual_advice = NO_ADVICE;
 
 void uart3MessageReceived(BMS_struct *BMS)
 {
@@ -18,45 +22,50 @@ void uart3MessageReceived(BMS_struct *BMS)
 		{
 		case 0:
 			actual_page = N_PAGE0;
-			NexPageShow(N_PAGE1);
+			NexPageShow(N_PAGE0);
 			break;
-
-		case 50:
-			actual_page = 50;
-			NexPageShow(N_PAGE4);
-			break;
-
-		case 51:
-		case 52:
-			BMS->config->ORDER = uart_user_message[1] - 51;
-			break;
-
-		case 53:
-			BMS->mode &= ~BMS_BALANCING;
-			for (int i = 0; i < N_OF_PACKS; ++i) {
-				LTC_reset_balance_flag(BMS->config, BMS->sensor[i]);
-				LTC_balance(BMS->config, BMS->sensor[i]);
-			}
-			break;
-
-		case 54:
-			BMS->mode |= BMS_BALANCING;
-			BMS->discharging = TRUE;
-			break;
-
-		default:
-			if(uart_user_message[1] > 0 && (uart_user_message[1] - 1) < N_OF_PACKS){
-				actual_page = uart_user_message[1];
-				NexPageShow(N_PAGE2);
-			}else if(uart_user_message[1] > 0 && ((uart_user_message[1] - N_OF_PACKS - 1) < N_OF_PACKS)){
-				actual_page = uart_user_message[1];
-				NexPageShow(N_PAGE3);
-			}
-			break;
-
 		}
 	}
 }
+//		case 50:
+//			actual_page = N_PAGE0;
+//			NexPageShow(N_PAGE0);
+//			break;
+//
+//		case 51:
+//		case 52:
+//			BMS->config->ORDER = uart_user_message[1] - 51;
+//			break;
+//
+//		case 53:
+//			BMS->mode &= ~BMS_BALANCING;
+//			for (int i = 0; i < N_OF_PACKS; ++i) {
+//				LTC_reset_balance_flag(BMS->config, BMS->sensor[i]);
+//				LTC_balance(BMS->config, BMS->sensor[i]);
+//			}
+//			break;
+//
+//		case 54:
+//			BMS->mode |= BMS_BALANCING;
+//			BMS->discharging = TRUE;
+//			break;
+//
+//		default:
+//			if(uart_user_message[1] > 0 && (uart_user_message[1] - 1) < N_OF_PACKS){
+//				actual_page = uart_user_message[1];
+//				NexPageShow(N_PAGE0);
+//			}else if(uart_user_message[1] > 0 && ((uart_user_message[1] - N_OF_PACKS - 1) < N_OF_PACKS)){
+//				actual_page = uart_user_message[1];
+//				NexPageShow(N_PAGE0);
+//			}
+//			break;
+//			actual_page = N_PAGE0;
+//			NexPageShow(N_PAGE0);
+//
+//			break;
+//		}
+//	}
+//}
 
 
 int cmpfunc (const void * a, const void * b) {
@@ -65,114 +74,140 @@ int cmpfunc (const void * a, const void * b) {
 
 void nexLoop(BMS_struct *BMS){
 
+	actual_page = N_PAGE0;
+
 	HAL_UART_DMAPause(&huart3);
 
-	if(NextError[0] == 1){
-		NexScrollingTextSetText(0, "Under Voltage");
-		NexScrollingTextSetPic(0, 11);
-	}
-	else if(NextError[1] == 1){
-		NexScrollingTextSetText(0, "Over Voltage");
-		NexScrollingTextSetPic(0, 11);
-	}
-	else if(NextError[2] == 1){
-		NexScrollingTextSetText(0, "Over Temperature");
-		NexScrollingTextSetPic(0, 11);
-	}
-	else if(NextError[3] == 1){
-		NexScrollingTextSetText(0, "Comm Error");
-		NexScrollingTextSetPic(0, 11);
-	}
-	else if(NextError[4] == 1){
-		NexScrollingTextSetText(0, "GLV Low Voltage");
-		NexScrollingTextSetPic(0, 11);
-	}
-	else{
-		NexScrollingTextSetText(0,"ALL OK!");
-		NexScrollingTextSetPic(0, 10);
-	}
+//	if(NextError[0] == 1){
+//		NexScrollingTextSetText(0, "Under Voltage");
+//		NexScrollingTextSetPic(0, 11);
+//	}
+//	else if(NextError[1] == 1){
+//		NexScrollingTextSetText(0, "Over Voltage");
+//		NexScrollingTextSetPic(0, 11);
+//	}
+//	else if(NextError[2] == 1){
+//		NexScrollingTextSetText(0, "Over Temperature");
+//		NexScrollingTextSetPic(0, 11);
+//	}
+//	else if(NextError[3] == 1){
+//		NexScrollingTextSetText(0, "Comm Error");
+//		NexScrollingTextSetPic(0, 11);
+//	}
+//	else if(NextError[4] == 1){
+//		NexScrollingTextSetText(0, "GLV Low Voltage");
+//		NexScrollingTextSetPic(0, 11);
+//	}
+//	else{
+//		NexScrollingTextSetText(0,"ALL OK!");
+//		NexScrollingTextSetPic(0, 10);
+//	}
+
+//	uint16_t buffer[N_OF_CELLS];
+
+//	switch(actual_page) /*actual_nextion_page is used to not use NexPageShow() many times*/
+//	{
+//	case N_PAGE0:
+//
+//		NexXfloatSetValue(0, BMS->v_max/10000);
+//		NexXfloatSetValue(1, BMS->v_min/10000);
+//		NexXfloatSetValue(2, BMS->v_TS/10);
+//		NexVariableSetValue(2, BMS->AIR);
+//		NexXfloatSetValue(3, BMS->t_max/10);
+//		NexNumberSetValue(0, 0);
+//		NexXfloatSetValue(1, BMS->v_GLV/100);
+//		NexXfloatSetValue(4, 1);
+//		NexProgressBarSetValue(0, BMS->charge_percent/10);
+
+//		NexPictureSetPic(0, 12 + HAL_GPIO_ReadPin(AIR_AUX_PLUS_GPIO_Port, AIR_AUX_PLUS_Pin));
+//		NexPictureSetPic(1, 12 + HAL_GPIO_ReadPin(AIR_AUX_MINUS_GPIO_Port, AIR_AUX_MINUS_Pin));
 
 
+//		break;
 
-	uint16_t buffer[N_OF_CELLS];
+//	case 50:
+//		NexWaveformAdd(1, 0, (int16_t)((float)(BMS->c_adc[0] - 2048) * 256/2048) + 128);
+//		NexWaveformAdd(1, 1, (int16_t)((float)(BMS->c_adc[2] - 2048) * 256/2048) + 128);
+//		NexWaveformAdd(1, 2, (int16_t)((float)(BMS->c_adc[3] - 2048) * 256/2048) + 128);
+//		//NexWaveformAdd(1, 3, (int16_t)((float)(BMS->c_adc[3] - 2048) * 256/2048) + 128);
+//		NexXfloatSetValue(0, (int16_t)BMS->current[0]);
+//		NexXfloatSetValue(1, (int16_t)BMS->current[2]);
+//		NexXfloatSetValue(2, (int16_t)BMS->current[3]);
+//		NexXfloatSetValue(3, (int16_t)BMS->current[1]/10);
+//
+//		break;
 
-	switch(actual_page) /*actual_nextion_page is used to not use NexPageShow() many times*/
-	{
-	case N_PAGE0:
+//	default:
+//		if(actual_page - 1 < N_OF_PACKS ){
+//
+//			NexProgressBarSetValue(0, BMS->sensor[actual_page - 1]->TOTAL_CHARGE/10);
+//
+//			for(uint8_t i = 0; i < N_OF_CELLS; i++)
+//				buffer[i] = BMS->sensor[actual_page - 1]->CxV[i];
+//
+//			if(BMS->config->ORDER)
+//				qsort(buffer, 12, sizeof(uint16_t), cmpfunc);
+//
+//			NexVariableSetValue(1,N_OF_PACKS);
+//			NexNumberSetValue(0,actual_page - 1);
+//
+//			for(uint8_t i = 0; i < N_OF_CELLS; i++){
+//				NexXfloatSetValue(i, buffer[i]);
+//				if((BMS->sensor[actual_page - 1]->DCC & (1 << i)) && !BMS->config->ORDER)
+//					NexXfloatSetCollor(i, 65504);
+//			}
+//
+//			NexXfloatSetValue(12,BMS->sensor[actual_page - 1]->GxV[4]);
+//			NexXfloatSetValue(13,BMS->sensor[actual_page - 1]->GxV[3]);
+//			NexXfloatSetValue(14,BMS->sensor[actual_page - 1]->GxV[2]);
+//			NexXfloatSetValue(15,BMS->sensor[actual_page - 1]->GxV[1]);
+//
+//		}else if(actual_page - N_OF_PACKS - 1 < N_OF_PACKS){
+//
+//			NexProgressBarSetValue(0, BMS->sensor[actual_page - N_OF_PACKS - 1]->TOTAL_CHARGE/10);
+//
+//			NexVariableSetValue(1,N_OF_PACKS);
+//			NexNumberSetValue(0,actual_page - N_OF_PACKS - 1);
+//			NexXfloatSetValue(0, BMS->sensor[actual_page - N_OF_PACKS - 1]->V_MAX);
+//			NexXfloatSetValue(1, BMS->sensor[actual_page - N_OF_PACKS - 1]->V_MIN);
+//			NexXfloatSetValue(2, BMS->sensor[actual_page - N_OF_PACKS - 1]->V_DELTA);
+//			NexXfloatSetValue(3, BMS->sensor[actual_page - N_OF_PACKS - 1]->SOC);
+//			NexXfloatSetValue(4, BMS->sensor[actual_page - N_OF_PACKS - 1]->ITMP);
+//			NexXfloatSetValue(5, (int16_t)BMS->current[0]);
+//			NexXfloatSetValue(6, (int16_t)BMS->current[2]);
+//			NexXfloatSetValue(7, (int16_t)BMS->current[3]);
+//			NexXfloatSetValue(8, (int16_t)BMS->current[1]);
+//
+//			//			NexXfloatSetValue(0, NexVariableGetValue(1));
+//		}
+//		break;
+//	}
+//	if(flag_information_to_send == 0){
+//		NexXfloatSetValue(0, 1);
+//		}
+//	else if(flag_information_to_send == 1){
+//			NexXfloatSetValue(1, BMS->v_min/10000);
+//		}
+//	else if(flag_information_to_send == 2){
+//			NexXfloatSetValue(2, BMS->v_TS/10);
+//		}
+//		else if(flag_information_to_send == 3){
+//			NexVariableSetValue(2, 1);
+//		}
+//		else if(flag_information_to_send == 4){
+//			NexXfloatSetValue(3, BMS->t_max/10);
+//		}
+//		else{
+//			flag_information_to_send = -1;
+//		}
+//		flag_information_to_send++;
+	NexXfloatSetValue(0, BMS->v_max/100);
+	NexXfloatSetValue(1, BMS->v_min/100);
+	NexXfloatSetValue(2, BMS->v_TS);
+	NexVariableSetValue(2, BMS->AIR);
+	NexXfloatSetValue(3, BMS->t_max);
 
-		NexVariableSetValue(0, BMS->AIR);
-		NexNumberSetValue(0, 0);
-		NexXfloatSetValue(0, BMS->v_TS/10);
-		NexXfloatSetValue(1, BMS->v_GLV/100);
-		NexXfloatSetValue(2, BMS->v_min);
-		NexXfloatSetValue(3, BMS->v_max);
-		NexXfloatSetValue(4, 1);
-		NexXfloatSetValue(5, BMS->t_max);
-		NexProgressBarSetValue(0, BMS->charge_percent/10);
-
-		NexPictureSetPic(0, 12 + HAL_GPIO_ReadPin(AIR_AUX_PLUS_GPIO_Port, AIR_AUX_PLUS_Pin));
-		NexPictureSetPic(1, 12 + HAL_GPIO_ReadPin(AIR_AUX_MINUS_GPIO_Port, AIR_AUX_MINUS_Pin));
-
-
-		break;
-
-	case 50:
-		NexWaveformAdd(1, 0, (int16_t)((float)(BMS->c_adc[0] - 2048) * 256/2048) + 128);
-		NexWaveformAdd(1, 1, (int16_t)((float)(BMS->c_adc[2] - 2048) * 256/2048) + 128);
-		NexWaveformAdd(1, 2, (int16_t)((float)(BMS->c_adc[3] - 2048) * 256/2048) + 128);
-		//NexWaveformAdd(1, 3, (int16_t)((float)(BMS->c_adc[3] - 2048) * 256/2048) + 128);
-		NexXfloatSetValue(0, (int16_t)BMS->current[0]);
-		NexXfloatSetValue(1, (int16_t)BMS->current[2]);
-		NexXfloatSetValue(2, (int16_t)BMS->current[3]);
-		NexXfloatSetValue(3, (int16_t)BMS->current[1]/10);
-
-		break;
-
-	default:
-		if(actual_page - 1 < N_OF_PACKS ){
-
-			NexProgressBarSetValue(0, BMS->sensor[actual_page - 1]->TOTAL_CHARGE/10);
-
-			for(uint8_t i = 0; i < N_OF_CELLS; i++)
-				buffer[i] = BMS->sensor[actual_page - 1]->CxV[i];
-
-			if(BMS->config->ORDER)
-				qsort(buffer, 12, sizeof(uint16_t), cmpfunc);
-
-			NexVariableSetValue(1,N_OF_PACKS);
-			NexNumberSetValue(0,actual_page - 1);
-
-			for(uint8_t i = 0; i < N_OF_CELLS; i++){
-				NexXfloatSetValue(i, buffer[i]);
-				if((BMS->sensor[actual_page - 1]->DCC & (1 << i)) && !BMS->config->ORDER)
-					NexXfloatSetCollor(i, 65504);
-			}
-
-			NexXfloatSetValue(12,BMS->sensor[actual_page - 1]->GxV[4]);
-			NexXfloatSetValue(13,BMS->sensor[actual_page - 1]->GxV[3]);
-			NexXfloatSetValue(14,BMS->sensor[actual_page - 1]->GxV[2]);
-			NexXfloatSetValue(15,BMS->sensor[actual_page - 1]->GxV[1]);
-
-		}else if(actual_page - N_OF_PACKS - 1 < N_OF_PACKS){
-
-			NexProgressBarSetValue(0, BMS->sensor[actual_page - N_OF_PACKS - 1]->TOTAL_CHARGE/10);
-
-			NexVariableSetValue(1,N_OF_PACKS);
-			NexNumberSetValue(0,actual_page - N_OF_PACKS - 1);
-			NexXfloatSetValue(0, BMS->sensor[actual_page - N_OF_PACKS - 1]->V_MAX);
-			NexXfloatSetValue(1, BMS->sensor[actual_page - N_OF_PACKS - 1]->V_MIN);
-			NexXfloatSetValue(2, BMS->sensor[actual_page - N_OF_PACKS - 1]->V_DELTA);
-			NexXfloatSetValue(3, BMS->sensor[actual_page - N_OF_PACKS - 1]->SOC);
-			NexXfloatSetValue(4, BMS->sensor[actual_page - N_OF_PACKS - 1]->ITMP);
-			NexXfloatSetValue(5, (int16_t)BMS->current[0]);
-			NexXfloatSetValue(6, (int16_t)BMS->current[2]);
-			NexXfloatSetValue(7, (int16_t)BMS->current[3]);
-			NexXfloatSetValue(8, (int16_t)BMS->current[1]);
-
-			//			NexXfloatSetValue(0, NexVariableGetValue(1));
-		}
-		break;
-	}
+//	NexPageShow(N_PAGE0);
 
 	HAL_UART_DMAResume(&huart3);
 
