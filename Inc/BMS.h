8516f3d2bae4f02da68503757e80e5ce5283f24b
Thiago Santos <47781320+thiagoolvr@users.noreply.github.com>
2022-03-30 17:23:05 -0300

    style: bms formatting

--- Inc/BMS.h
+++ Inc/BMS.h
@@ -1,21 +1,16 @@
-/****************************************************************/
-/**					Team Formula Tesla UFMG - 2018				*/
-/** Microcontroller: STM32F103XXXX								*/
-/** Compiler: AC6 - STM worbench								*/
-/** Author: Henrique, Eric, Virginia							*/
-/** License: Free - Open Source									*/
-/** 															*/
-/****************************************************************/
+/*
+ * bms.h
+ *
+ *  Created on: 17 de mar de 2022
+ *      Author: Thiago
+ */
+
 #ifndef BMS_H
 #define BMS_H
 
-#include "LTC6804.h"
-#include "DHAB_s125.h"
-#include "eeprom.h"
-#include "can.h"
+#include "ltc.h"
 #include "defines.h"
-
-uint8_t NextError[5];
+#include "stdlib.h"
 
 typedef struct BMS_struct {
 
@@ -29,7 +24,7 @@ typedef struct BMS_struct {
 
 	LTC_sensor *sensor[N_OF_SLAVES];
 	LTC_config *config;
-	DHAB_sensor *dhabSensor[N_OF_DHAB];
+//	DHAB_sensor *dhabSensor[N_OF_DHAB];
 
 	uint16_t v_GLV;
 	uint16_t v_TS;
@@ -42,18 +37,8 @@ typedef struct BMS_struct {
 	float current[4];
 	float c_adc[4];
 
-	//CHARGE_PERCENTAGE
-	int32_t charge;
-	float charge_percent;
-	int32_t charge_max;
-	int32_t charge_min;
-	float charge_variation_percent;
-	float discharge_percent;
-	float discharge_variation_percent;
-
 	//AIR
 	uint8_t AIR;
-
 }BMS_struct;
 
 typedef enum{
@@ -69,30 +54,12 @@ typedef enum{
 #define BMS_CONVERT_STAT	4
 #define BMS_CONVERT_CONFIG	8
 
-void BMS_mode_selector(BMS_struct *BMS);
-
 void BMS_Init(BMS_struct *BMS);
-
-void BMS_Convert(uint8_t BMS_CONVERT, BMS_struct *BMS);
-
+void BMS_SetSafetyLimits(BMS_struct* BMS);
 void BMS_Monitoring(BMS_struct *BMS);
-
-uint8_t BMS_AIR_status(BMS_struct *BMS);
-
-void BMS_charging(BMS_struct BMS);
-
-void BMS_discharging(BMS_struct BMS);
-
-int BMS_balance(BMS_struct *BMS);
-
-int BMS_communication(BMS_struct *BMS);
-
+void BMS_Convert(uint8_t BMS_CONVERT, BMS_struct *BMS);
+void BMS_Balance(BMS_struct *BMS);
+void BMS_AIR_status(BMS_struct *BMS);
 void BMS_ErrorTreatment(BMS_struct *BMS);
-
-void BMS_can(BMS_struct *BMS);
-
-void BMS_uart(BMS_struct *BMS);
-
-void BMS_initial_SOC(BMS_struct *BMS);
 
 #endif
